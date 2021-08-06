a70cbf4e74ddbb663eaaa441249fc6904eceada9
Gnomeball <gnome.c@protonmail.com>
2021-08-06 14:19:00 +0100

    Culled logging messages a lile + changed the date string to include ordinals.

--- bot.py
+++ bot.py
@@ -2,18 +2,19 @@ import time, discord, asyncio, random, logging, sys
 from datetime import datetime
 from quotes import pull_random_quote, pull_quotes_from_repo, pull_quotes_from_file, refresh_quotes
 
-# logging boilerplate
+# Logging boilerplate
 fmt = '[%(asctime)s: %(name)s %(levelname)s]: %(message)s'
-logging.basicConfig(level = logging.INFO, stream = sys.stdout, format = fmt)
+logging.basicConfig(level = logging.WARNING, stream = sys.stdout, format = fmt)
 
 # Variables and stuff
 client = discord.Client()
+logger = logging.getLogger("QuoteBot")
+
 gnome = 356467595177885696
 sandbox = 767326418572148756
-logger = logging.getLogger("QuoteBot")
 colours = [0xc27c0e, 0x992d22, 0xad1457, 0x71368a, 0x206694, 0x11806a]
-MINUTE = 60
 
+MINUTE = 60
 REPO_LINK = "https://github.com/Gnomeball/QuoteBotRepo"
 
 # Quotes
@@ -50,12 +51,18 @@ async def quote_loop():
             await send_quote()
 
 @client.event
+async def current_date_time():
+    day_n = datetime.now().day
+    day_ord = "th" if 4 <= day_n % 100 <= 20 else {1:"st", 2:"nd", 3:"rd"}.get(day_n % 10, "th")
+    return datetime.now().strftime('%A %-d# %B %Y').replace("#", day_ord)
+
+@client.event
 async def send_quote(pre = "Quote"):
     quotes = await refresh_quotes() # This way we have access to the latest quotes
     logger = logging.getLogger("send_quote")
     submitter, quote = pull_random_quote(quotes)
     embedVar = discord.Embed(title = "Maximum Swack!", description = quote, colour = random.choice(colours))
-    embedVar.set_footer(text = f"{pre} for {datetime.now().strftime('%A %-d %B %Y')}\nSubmitted by {submitter}")
+    embedVar.set_footer(text = f"{pre} for {current_date_time()}\nSubmitted by {submitter}")
     logger.info(f"Sending quote from {submitter}: {quote}")
     await client.get_channel(sandbox).send(embed = embedVar)
 

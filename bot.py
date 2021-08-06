232744c328c04e5f6e20984d20b3fc33a59f7947
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2021-08-06 14:43:43 +0100

    Docs and attribution

--- bot.py
+++ bot.py
@@ -59,7 +59,7 @@ async def send_quote(pre = "Quote"):
     quote = pull_random_quote(quotes)
     quote_text = quote.quote
     if quote.attribution is not None: 
-      quote_text += f"~{quote.attribution}"
+      quote_text += f" ~{quote.attribution}"
     embedVar = discord.Embed(title = "Maximum Swack!", description = quote_text, colour = random.choice(colours))
     embedVar.set_footer(text = f"{pre} for {current_date_time()}\nSubmitted by {quote.submitter}")
     logger.info(f"Sending quote from {quote.submitter}: {quote_text}")

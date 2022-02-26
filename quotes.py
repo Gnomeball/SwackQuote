a8694a9239351a0cf5f1a2367ce219bfa7683cbc
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-02-26 14:37:32 +0000

    Quote Deck online, like the Steam Deck only nothing like it at all

--- quotes.py
+++ quotes.py
@@ -7,7 +7,10 @@ import tomli
 import tomli_w
 
 QUOTE_FILE_ADDRESS = 'https://raw.githubusercontent.com/Gnomeball/QuoteBotRepo/main/quotes.toml'
-QUOTE_FILE_PATH = "quotes.toml"
+QUOTE_FILE_PATH = "quotes.toml" # The collection of all quotes
+QUOTE_DECK_PATH = "quote_deck.txt" # The current deck of quotes we're using
+QUOTE_HISTORY_PATH = "quote_history.txt" # The logged appearances of each quote
+QUOTE_REPEAT_DELAY = 200 # How many days must pass before a repeated quote should be allowed
 
 # Our Quote type, has optional attribution & source, requires submitter & quote
 Quote = namedtuple("Quote", "submitter quote attribution source", defaults=(None, None))
@@ -31,19 +34,26 @@ def pull_specific_quote(quote: str, quotes: dict):
 def pull_random_quote(quotes: dict):
     """
     Selects a random quote from the given dictionary.
-    Currently, ignores the last 100 quotes logged in "quote_history.txt".
+    Currently, ignores the last QUOTE_REPEAT_DELAY quotes.
+    We reference the deck of current quotes for which are good to use and update it.
     :returns: A Quote(submitter, quote, attribution=None, source=None) and its position in the full list.
     :rtype: Quote, int
     """
-    with open("quote_history.txt", "r", encoding="utf8") as f:
-        recent = set(map(str.strip, f.readlines()[-100:]))
-        good_q = [(i,k) for i,k in enumerate(quotes.keys(),1) if k not in recent]
+    with open(QUOTE_DECK_PATH, "r", encoding="utf8") as d:
+        deck = set(map(str.strip, d.readlines()))
+
+    with open(QUOTE_HISTORY_PATH, "r", encoding="utf8") as f:
+        recent = set(map(str.strip, f.readlines()[-QUOTE_REPEAT_DELAY:]))
+        good_q = [(i,k) for i,k in enumerate(quotes.keys(), 1) if k not in recent and k in deck]
 
     random.shuffle(good_q)
     quote_index, quote = random.choice(good_q)
+    deck.remove(quote)
 
-    with open("quote_history.txt", "a", encoding="utf8") as f:
+    with open(QUOTE_HISTORY_PATH, "a", encoding="utf8", newline="\n") as f:
         f.write(f"{quote}\n")
+    with open(QUOTE_DECK_PATH, "w+", encoding="utf8", newline="\n") as d:
+        d.write("\n".join(deck-recent))
 
     return (Quote(**quotes[quote]), quote_index)
 
@@ -54,7 +64,7 @@ def pull_quotes_from_file(path=QUOTE_FILE_PATH):
     :rtype: Dict
     """
     with open(path, "r", encoding="utf8") as f:
-        return tomli.load(f)
+        return tomli.loads(f.read())
 
 def pull_quotes_from_repo():
     """
@@ -68,7 +78,6 @@ def pull_quotes_from_repo():
     try:
         logger.info(f"Updating quotes from: {QUOTE_FILE_ADDRESS}")
         req = requests.get(QUOTE_FILE_ADDRESS)
-        # error codes don't throw exceptions, for some reason
         if req.status_code != 200:
             logger.error(f"Failed to get {QUOTE_FILE_ADDRESS} with status: {req.status_code}")
         else: updated_quotes = req.text
@@ -105,4 +114,12 @@ async def refresh_quotes():
     if quotes != updated_quotes:
         with open(QUOTE_FILE_PATH, "wb") as f:
             tomli_w.dump(updated_quotes, f)
+        with open(QUOTE_DECK_PATH, "w+", encoding="utf8", newline="\n") as d:
+            deck = set(map(str.split, d.readlines()))
+            if len(deck) == 0: # Cycle deck, filling it back up again
+              d.write("\n".join(updated_quotes.keys()))
+            else:
+              deck |= set(additions)
+              deck -= set(removals)
+              d.write("\n".join(deck))
     return updated_quotes

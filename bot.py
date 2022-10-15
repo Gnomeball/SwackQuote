822d20f367720583aaabe762a15f158938a560cd
Gnomeball <gnome.c@protonmail.com>
2022-10-15 20:05:04 +0100

    Logger logger is logging.getLogger(Logging).as(Logging.class)

--- bot.py
+++ bot.py
@@ -7,61 +7,69 @@ import tomli
 
 from quotes import pull_random_quote, pull_specific_quote, refresh_quotes, format_quote_text, calculate_swack_level, QUOTE_FILE_PATH, QUOTE_DUD_PATH, QUOTE_DECK_PATH, QUOTE_HISTORY_PATH
 
-# Logging boilerplate
+# * Logging boilerplate
 fmt = "[%(asctime)s: %(name)s %(levelname)s]: %(message)s"
 logging.basicConfig(level = logging.INFO, stream = sys.stdout, format = fmt)
 
-# Variables and stuff
+# * Variables and stuff
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents = intents)
 logger = logging.getLogger("QuoteBot")
 
-# Those able to send commands to the bot and which channel it must be in
+# * Those able to send commands to the bot and which channel it must be in
 ADMINS  = set(tomli.loads(Path("admins.toml").read_text()).values())
 CHANNEL = int(Path("channel.txt").read_text())
 
 MINUTE = 60
 REPO_LINK = "https://github.com/Gnomeball/SwackQuote"
 
-# Random colours for the embed
+# * Random colours for the embed
 
-def random_colour_hex():
+def random_colour() -> int:
+    """
+    Calculates a random (ish) Hex colour - Used to make embeds a little less boring.
+    :returns: integer value for a colour.
+    :rtype: int
+    """
     colour = colorsys.hsv_to_rgb(random.random(), random.uniform(0.42, 0.98), random.uniform(0.4, 0.9))
-    return "0x" + "".join(hex(int(x*255))[2:].zfill(2) for x in colour)
+    colour_hex = "0x" + "".join(hex(int(x*255))[2:].zfill(2) for x in colour)
+    return int(colour_hex, 16)
 
-def random_colour():
-    return int(random_colour_hex(), 16)
-
-# Client events
+# * Client events
 
 @client.event
 async def on_ready():
     logging.info(f"We have logged in as {client.user}")
-    # await client.change_presence(activity = discord.CustomActivity("Blah"))
     await client.change_presence(activity = discord.Game(name = "Selecting a quote!"))
 
 @client.event
 async def on_message(message: discord.Message):
+    logger = logging.getLogger("on_message")
     if message.channel.id == CHANNEL:
         if message.author.id in ADMINS:
             if message.content == "#reroll":
-                await send_quote("Re-rolled Quote")
+                logger.info(f"Requesting quote re-roll")
+                await send_quote("Re-rolled Quote", log="quote_request")
             if message.content[:5] == "#test":
-                await test_quote(message.content[5:].strip())
+                logger.info(f"Requesting test quote")
+                await test_quote(message.content[5:].strip(), log="quote_request")
         if message.content == "#repo":
             await client.get_channel(CHANNEL).send(content = REPO_LINK)
-    else: pass
+    else:
+        pass
 
 @client.event
 async def dud_quotes():
     logger = logging.getLogger("dud_quotes")
-    duds = Path(QUOTE_DUD_PATH).read_text().strip() # we just print verbatim, no need to parse
+    # * We just print verbatim, no need to parse
+    duds = Path(QUOTE_DUD_PATH).read_text().strip()
     if len(duds):
         logger.info(f"Sending dud quotes: \n{duds}")
         embedVar = discord.Embed(title = "These quotes need fixing", description = f"```toml\n{duds[:4000]}\n```", colour = random_colour())
         await client.get_channel(CHANNEL).send(embed = embedVar)
+        logger.info(f"Dud quotes have been sent")
     else:
         logger.info("There were no dud quotes today")
 
@@ -85,26 +93,37 @@ async def current_date_time():
 
 @client.event
 async def send_quote(pre: str = "Quote", title: Optional[str] = None, which: Optional[str] = None, log: str = "send_quote"):
-    quotes = await refresh_quotes() # This way we have access to the latest quotes
     logger = logging.getLogger(log)
+
+    quotes = await refresh_quotes() # This way we have access to the latest quotes
     await dud_quotes()
+
     quote, i = pull_random_quote(quotes) if which is None else pull_specific_quote(which, quotes)
     quote_text = format_quote_text(quote)
     title = calculate_swack_level() if title is None else title
+
+    # * Build the embed
     embedVar = discord.Embed(title = title, description = quote_text, colour = random_colour())
     if quote.source:
       embedVar.url = quote.source
     if quote.quote == quote.source:
       embedVar._video = {"url": quote.quote}
     embedVar.set_footer(text = f"{pre} for {await current_date_time()}\nQuote {i}/{len(quotes)}, Submitted by {quote.submitter}")
+
+    # * Try and send the embed
     logger.info(f"Sending quote from {quote.submitter}: {quote_text}")
-    await client.get_channel(CHANNEL).send(embed = embedVar)
+    try:
+        await client.get_channel(CHANNEL).send(embed = embedVar)
+    except e:
+        logger.info(f"Error sending quote : {str(e)}")
+    finally:
+        logger.info(f"Quote sent successfully")
 
 @client.event
-async def test_quote(which = "pre-toml-255"):
-    await send_quote(pre = "Testing", title = "Testing the Swack", which = which, log = "test_quote")
+async def test_quote(which = "pre-toml-255", log: str = "test_quote"):
+    await send_quote(pre = "Testing", title = "Testing the Swack", which = which, log = log)
 
-# Run the thing
+# * Run the thing
 
 QUOTE_FILE_PATH.touch()
 QUOTE_DUD_PATH.touch()
